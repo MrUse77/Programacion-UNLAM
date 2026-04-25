4cc44820d0dc5045e1c6628766a7676eb7336264
MrUse77 <agusdor14@hotmail.com>
2026-04-25 18:55:58 -0300

    feat: Cola Dinamica

--- Algoritmos/includes/Cola.h
+++ Algoritmos/includes/Cola.h
@@ -6,9 +6,11 @@
 #define FALSE 0
 #define OK 1
 #define ERR 2
+#define ERR_MEM 101
 #define ERR_LLENA 301
 #define ERR_VACIA 302
 
+#ifdef COLA_ESTATICA
 #define TAM_COLA 10000
 typedef struct {
   char Vector[TAM_COLA];
@@ -16,6 +18,17 @@ typedef struct {
   unsigned ult;
   unsigned tamDisp;
 } t_Cola;
+#else
+
+typedef struct sNodo { // sNodo defino la estructura
+  void *dato;
+  unsigned tamDato;
+  struct sNodo *sig;
+} t_Nodo; // inicializo el tipo de dato
+typedef struct {
+  t_Nodo *prim, *ult;
+} t_Cola;
+#endif
 
 void crearCola(t_Cola *c);
 int ponerEnCola(t_Cola *c, const void *d, unsigned tamDato);
